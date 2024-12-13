1b489ca0b589e39ca610fd384cff3d3d16af6111
mramirezcamacho <mramirezcamacho_i@didiglobal.com>
2024-12-13 14:36:16 -0500

    input format added

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from makeFilesForGattaranUpload import divide_per_country
 from automater import doSelenium
 from allMLStuff import predictFile
+import sys
+
 
 if __name__ == '__main__':
-    # predictFile('NewRankDataset_cristobalnavarro_20241129002848.csv')
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+        if '.csv' not in file:
+            file = file+'.csv'
+    else:
+        print("No input file provided, using default file.")
+        file = 'NewRankDataset_cristobalnavarro_20241129002848.csv'
+
+    predictFile(file)
     divide_per_country()
     doSelenium()
